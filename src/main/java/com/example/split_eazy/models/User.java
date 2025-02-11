0358652f2fe6741189c639fa45ab0fc8dfee4c44
CarahPrinsloo <carahprinsloo@gmail.com>
2025-02-11 08:22:52 +0200

    added database configuration and flyway migrations for users table in split eazy db

---
+++ src/main/java/com/example/split_eazy/models/User.java
@@ -0,0 +1,18 @@
+package com.example.split_eazy.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    Integer id;
+    UUID uuid;
+    String name;
+}
